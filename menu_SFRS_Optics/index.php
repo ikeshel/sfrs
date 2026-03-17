bc22a4baf2a5581bdc4811ca95e5851760009446
Irakli Keshelashvili <i.keshelashvili@gsi.de>
2026-03-17 11:41:15 +0100

    new files

--- menu_SFRS_Optics/index.php
+++ menu_SFRS_Optics/index.php
@@ -6,10 +6,22 @@
 ?>
 </head>
 <body>
+    <div class="menu-buttons" style="height: 50px; padding: 20px; background-color: #f0f0f0;">
+        <button onclick="changeSVG('S-FRS_Detectors_V.1.43_08.02.2023_1')">V.1.43</button>
+        <button onclick="changeSVG('legend.webp')">Legend</button>
+        <button onclick="changeSVG('minimal')">Minimal</button>
+        <button onclick="changeSVG('only_first')">Only First</button>
+    </div>
+
+    <script>
+    function changeSVG(filename) {
+        document.querySelector('object[type="image/svg+xml"]').setAttribute('data', filename);
+    }
+    </script>
     <div class="container">
         <div class="svg-wrapper">
-            <object type="image/svg+xml" data="S-FRS_Detectors_V.1.43.svg">
-                <img src="S-FRS_Detectors_V.1.43.svg" alt="S-FRS_Detectors_V.1.43" />
+            <object type="image/svg+xml" data="S-FRS_Detectors_V.1.43_08.02.2023_1">
+                <img src="" alt="" />
             </object>
         </div>
     </div>
